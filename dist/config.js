3b7ce531106a131de2317c0e6e86f631a38ba98c
aryaman1706 <1761ary@gmail.com>
2022-08-20 22:29:59 +0530

    removed dist

--- dist/config.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = {
-    SUPABASE_URL: process.env.SUPABASE_URL,
-    SUPABASE_KEY: process.env.SUPABASE_KEY,
-    JWT_SECRET: process.env.JWT_SECRET,
-};
-//# sourceMappingURL=config.js.map
