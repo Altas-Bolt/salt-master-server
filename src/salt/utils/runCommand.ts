ac5b4ae6e7b1e879d2f115fc2d1cdcaa0c6d9099
aryaman1706 <1761ary@gmail.com>
2022-08-23 04:19:52 +0530

    -

--- src/salt/utils/runCommand.ts
+++ src/salt/utils/runCommand.ts
@@ -5,7 +5,7 @@ const runCmd = async (cmd: string) => {
   const promisifiedExec = promisify(exec);
   const { stdout, stderr } = await promisifiedExec(cmd);
 
-  if (stderr) return Promise.reject(stderr);
+  if (stderr) return stderr;
   return stdout;
 };
 
